19573db3970b819b54ba02877f807d541b38aa14
pitrix <vidabonney278@gmail.com>
2022-08-24 19:03:20 +0000

    ...

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -27,7 +27,7 @@ const Upload = () => {
 
   const uploadVideo = async (e: any) => {
     const selectedFile = e.target.files[0];
-    const fileTypes = ['video/mp4', 'video/webm', 'video/ogg'];
+    const fileTypes = ['video/mp4', 'video/webm', 'video/ogg', 'image/png', 'image/jpeg', 'image/jpg', 'image/gif'];
 
     // uploading asset to sanity
     if (fileTypes.includes(selectedFile.type)) {
