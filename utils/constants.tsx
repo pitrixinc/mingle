36496698cb518e6465592020cf638e0b01d52bf8
pitrixinc <vidabonney278@gmail.com>
2022-10-05 10:41:26 +0000

    ...

--- utils/constants.tsx
+++ utils/constants.tsx
@@ -37,6 +37,6 @@ export const topics = [
   },
 ];
 
-export const footerList1 = ['About', 'Newsroom', 'Store', 'Contact', 'Carrers', 'ByteDance', 'Creator Directory']
-export const footerList2 = [ 'TikTik for Good','Advertise','Developers','Transparency','TikTik Rewards' ]
+export const footerList1 = ['About', 'Newsroom', 'Store', 'Contact', 'Carriers', 'ByteDance', 'Creator Directory']
+export const footerList2 = [ 'Mingle for Good','Advertise','Developers','Transparency','Mingle Rewards' ]
 export const footerList3 = [ 'Help', 'Safety', 'Terms', 'Privacy', 'Creator Portal', 'Community Guidelines' ]
