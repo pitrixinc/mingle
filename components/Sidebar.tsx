e6a1a91e3a79f296acb6ae66001592fb8c1e0019
pitrixinc <vidabonney278@gmail.com>
2022-11-04 20:43:51 +0000

    ...

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -24,7 +24,7 @@ const Sidebar: NextPage = () => {
   return (
     <div>
       <div
-        className='md:block hidden m-2 ml-1 mt-3 text-xl'
+        className='md:block sm:block hidden m-2 ml-1 mt-3 text-xl'
         onClick={() => setShowSidebar(!showSidebar)}
       >
         {showSidebar ? <ImCancelCircle /> : <AiOutlineMenu />}
