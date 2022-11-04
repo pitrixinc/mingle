87f3f4e970a258fa290e32e0d5fcc54b58ecf953
pitrixinc <vidabonney278@gmail.com>
2022-11-04 20:31:41 +0000

    ...

--- utils/constants.tsx
+++ utils/constants.tsx
@@ -38,6 +38,6 @@ export const topics = [
   },
 ];
 
-export const footerList1 = [<a href="./components/about-us-1.html">About</a>, 'Newsroom', 'Store', 'Contact', 'Carriers', 'ByteDance', 'Creator Directory']
+export const footerList1 = ['About', 'Newsroom', 'Store', 'Contact', 'Carriers', 'ByteDance', 'Creator Directory']
 export const footerList2 = [ 'Mingle for Good','Advertise','Developers','Transparency','Mingle Rewards' ]
 export const footerList3 = [ 'Help', 'Safety', 'Terms', 'Privacy', 'Creator Portal', 'Community Guidelines' ]
