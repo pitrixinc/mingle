5a83aa82fa30aef8e3d7e527ada990d92417e2db
pitrixinc <vidabonney278@gmail.com>
2022-11-06 15:24:10 +0000

    ...

--- components/MiniNavbar.tsx
+++ components/MiniNavbar.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import Router, { useRouter } from 'next/router';
 import { GoogleLogin, googleLogout } from '@react-oauth/google';
 import { AiOutlineLogout } from 'react-icons/ai';
-import { BiSearch } from 'react-icons/bi';
+import { BiSearch, BiVideoPlus } from 'react-icons/bi';
 import { IoMdAdd } from 'react-icons/io';
 
 import Logo from '../utils/mingle.png';
@@ -55,67 +55,30 @@ const MiniNavbar = () => {
             </Link>
           </div>
 
-        <div className="relative block">
-          <form
-            onSubmit={handleSearch}
-            className="absolute md:static top-10 -left-20 bg-white"
-          >
-            <input 
-              type="text"
-              value={searchValue}
-              onChange={(e) => setSearchValue(e.target.value)}
-              placeholder="Search Accounts and Videos"
-              className="bg-white p-3 md:text-md font-medium border-2 border-gray-140 focus:outline-none focus:border-2 focus:border-gray-300 w-[300px] md:w-[350px] rounded-full md:top-0"
-            />
-            <button
-              onClick={handleSearch}
-              className="absolute md:right-5 right-6 top-4 border-l-2 border-gray-300 pl-4 text-2xl text-gray-400"
-            >
-              <BiSearch/>
-            </button>
-          </form>
-        </div>
+          <div className="xl:border-b-2 border-gray-200 pb-1">
+                 <Link href="/">
+                  <div className="flex items-center gap-1 hover:bg-primary p-3 justify-center xl:justify-start cursor-pointer font-bold text-black rounded">
+                    <p className="text-2xl">
+                      <BiSearch />
+                      </p>
+                      
+                  </div>
+                 </Link>
+              </div>
 
-        <div>
-          {userProfile ? (
-            <div className="flex gap-5 md:gap-10">
-                <Link href="/upload">
-                   <button className="border-2 px-2 md:px-4 text-md font-semibold flex items-center gap-2">
-                       <IoMdAdd className="text-xl" /> {` `}
-                       <span className="hidden md:block">Upload</span>
-                   </button>
-                </Link>
-                {userProfile.image && (
-                  <Link href="/">
-                  <>
-                  <Image
-                    width={40}
-                    height={40}
-                    className="rounded-full cursor-pointer"
-                    src={userProfile.image}
-                    alt="profile photo"
-                  />
-                  </>
-                </Link>
-                )}
-                <button
-                  type="button"
-                  className="px-2"
-                  onClick={() => {
-                    googleLogout();
-                    removeUser();
-                  }}
-                  >
-                   <AiOutlineLogout color="red" fontSize={21} /> 
-                </button>
-            </div>
-          ) : (
-            <GoogleLogin
-               onSuccess={(response) => createOrGetUser(response, addUser)}
-               onError={() => console.log('Error')}
-            />
-          )}
-        </div>
+              <div className="xl:border-b-2 border-gray-200 pb-1">
+                 <Link href="https://mymingle.netlify.app">
+                  <div className="flex items-center gap-1 hover:bg-primary p-3 justify-center xl:justify-start cursor-pointer font-bold text-black rounded">
+                    <p className="text-2xl">
+                      <BiVideoPlus />
+                      </p>
+                      <span className="text-md hidden xl:block">
+                        Video Meeting
+                      </span>
+                  </div>
+                 </Link>
+              </div>
+
     </div>
   )
 }
