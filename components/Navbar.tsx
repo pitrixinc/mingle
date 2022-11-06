90c6ff79f74963680a7ed57b551c9a809475b2bb
pitrixinc <vidabonney278@gmail.com>
2022-11-06 15:33:38 +0000

    ...

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -27,7 +27,7 @@ const Navbar = () => {
   }
 
   return (
-    <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
+    <div className="w-full flex justify-between items-center py-2 px-4">
         <Link href="/">
            <div className="w-[100px] md:w-[130px]">
               <Image 
