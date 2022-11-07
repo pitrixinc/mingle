8c2a7c531699d128143e2f2bd05a77e3106b84b6
pitrixinc <vidabonney278@gmail.com>
2022-11-07 19:48:10 +0000

    ...

--- components/MiniDiscover.tsx
+++ components/MiniDiscover.tsx
@@ -10,12 +10,12 @@ const MiniDiscover = () => {
 
     const activeTopicStyle = "border-2 hover:bg-primary border-[#F51997] px-3 py-2 rounded rounded-full flex items-center gap-2 justify-center cursor-pointer text-[#F51997] "
 
-    const topicStyle ="border-2 hover:border-[#F51997] border-gray-300 px-3 py-2 rounded rounded-full flex items-center gap-2 justify-center cursor-pointer text-black hover:text-white hover:bg-[#F51997]"
+    const topicStyle ="p-4 border-2 hover:border-[#F51997] border-gray-300 px-3 py-2 rounded rounded-full flex items-center gap-2 justify-center cursor-pointer text-black hover:text-white hover:bg-[#F51997]"
 
   return (
     <div className="border-b-2 border-gray-200 pb-6 mb-0">
       
-      <div className="flex gap-3 flex-row">
+      <div className="flex gap-0 flex-row">
         {topics.map((item) => (
               <Link href={`/?topic=${item.name}`} key={item.name}>
                 <div className={topic === item.name ? activeTopicStyle : topicStyle}>
