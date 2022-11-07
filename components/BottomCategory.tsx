3499fa45e0cd4c6b5bc6032e527ab81edef05e1d
pitrixinc <vidabonney278@gmail.com>
2022-11-07 18:17:56 +0000

    ...

--- components/BottomCategory.tsx
+++ components/BottomCategory.tsx
@@ -26,18 +26,23 @@ const BottomCategory = () => {
    .slice(0, allUsers.length);
 
   return (
-    <div className="md:hidden xl:hidden block  pb-4 flex mr-0">
+    
     <section
   className="block fixed bottom-0 inset-x-0 z-50 shadow-lg text-gray-800 bg-gray-700 dark:bg-dark backdrop-blur-lg bg-opacity-30 dark:bg-opacity-30 dark:text-gray-400 border-t-2 border-royal/20">
   <div id="tabs" className="flex justify-between">
        
-        
+  <div className="md:hidden xl:hidden block  pb-4 flex mr-0">
         <div className="flex overflow-x-auto space-x-1 snap-x">
+        <div className="gap-1 hover:bg-primary p-2 cursor-pointer font-semibold rounded">
+                  <div className="w-29 h-29 md:w-40 md:h-40">
           <Discover />
+          </div>
+          </div>
         </div>
-   </div>     
+   </div>   
+   </div>  
 </section>
-</div>
+
   )
 }
 
