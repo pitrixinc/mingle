6bb0968312c7bb542c23759b238f246e0ff1a914
pitrixinc <vidabonney278@gmail.com>
2022-11-08 07:29:31 +0000

    ...

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -26,7 +26,7 @@ const Comments = ({ comment, setComment, addComment, comments, isPostingComment 
   const { allUsers, userProfile }: any = useAuthStore();
 
   return (
-    <div className='border-t-2 border-gray-200 pt-4 px-10 mt-4 bg-[#F8F8F8] border-b-2 lg:pb-0 pb-[100px]'>
+    <div className='border-t-2 border-gray-200 pt-4 px-10 mt-4 bg-[#F8F8F8] border-b-2 lg:pb-0 pb-[100px] sm:mb-40'>
       <div className='overflow-scroll lg:h-[457px]'>
         {comments?.length > 0 ? (
           comments?.map((item: IComment, idx: number) => (
