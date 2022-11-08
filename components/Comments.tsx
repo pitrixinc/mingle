3d41a1c2c45381671e056a29cb48c3689fb468f0
pitrixinc <vidabonney278@gmail.com>
2022-11-08 07:42:10 +0000

    ...

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -76,7 +76,7 @@ const Comments = ({ comment, setComment, addComment, comments, isPostingComment 
             className='bg-primary px-6 py-4 text-md font-medium border-2 w-[250px] md:w-[700px] lg:w-[350px] border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 flex-1 rounded-lg '
             placeholder='Add comment..'
           />
-          <button className='text-md text-gray-400 sm:mb-5' onClick={addComment}>
+          <button className='text-md text-gray-400' onClick={addComment}>
             {isPostingComment ? 'Commenting...' : 'Comment'}
           </button>
         </form>
