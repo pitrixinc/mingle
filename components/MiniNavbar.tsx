6e51df7eca7a48f471fd1cea40034ffe68ed84e4
pitrixinc <vidabonney278@gmail.com>
2022-11-08 09:46:48 +0000

    ...

--- components/MiniNavbar.tsx
+++ components/MiniNavbar.tsx
@@ -61,7 +61,7 @@ const MiniNavbar = () => {
               </div>
 
           <div className="xl:border-b-2 border-gray-200 pb-1">
-                 <Link href="./MiniSearch">
+                 <Link href="/components/MiniSearch.tsx">
                   <div className="flex items-center gap-1 hover:bg-primary p-3 justify-center xl:justify-start cursor-pointer font-bold text-black rounded">
                     <p className="text-2xl">
                       <BiSearch />
