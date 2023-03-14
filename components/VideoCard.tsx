0539ddbe1e62145494a1ce380ae8c0f2a0b725e2
pitrixinc <vidabonney278@gmail.com>
2023-03-14 19:23:14 +0000

    '...'

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -203,7 +203,7 @@ const VideoCard: NextPage<IProps> = ({ post}: IProps) => {
               
               <div className="pb-1">
                   <div className="flex items-center gap-1 hover:bg-primary p-3 justify-center xl:justify-start cursor-pointer font-bold text-black rounded">
-                    <a href={post.video.asset.url} download>
+                    <a href={post.video.asset.url} download="">
                       <button className="text-2xl">
                         <FiDownload />
                        </button>
